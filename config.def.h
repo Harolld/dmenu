5231925b44c210e9cbd16b8b8f8f5567aec8a400
harold <harold@thinkpad.localdomain>
2022-03-17 02:39:39 +0100

    Applied alpha patch

--- config.def.h
+++ config.def.h
@@ -2,8 +2,9 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
+static const unsigned int alpha = 120;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -15,9 +16,14 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+static const unsigned int alphas[SchemeLast][2] = {
+    [SchemeNorm] = { OPAQUE, alpha },
+    [SchemeSel] = { OPAQUE, alpha },
+    [SchemeOut] = { OPAQUE, alpha },
+};
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 0;
-static unsigned int columns    = 0;
+static unsigned int lines      = 10;
+static unsigned int columns    = 5;
 
 /*
  * Characters not considered part of a word while deleting words
@@ -26,4 +32,4 @@ static unsigned int columns    = 0;
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 0;
+static unsigned int border_width = 3;
--- config.h
+++ config.h
@@ -4,9 +4,10 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
+static const unsigned int alpha = 120;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Terminus:size=24"
+	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
@@ -14,6 +15,11 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
+};
+static const unsigned int alphas[SchemeLast][2] = {
+    [SchemeNorm] = { OPAQUE, alpha },
+    [SchemeSel] = { OPAQUE, alpha },
+    [SchemeOut] = { OPAQUE, alpha },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 10;
